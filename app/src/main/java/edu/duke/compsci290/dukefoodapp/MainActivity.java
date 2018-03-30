085d4f685835c5e6def012670c327ce792fb75c7
Tanner Johnson <tlj21@duke.edu>
2018-03-30 13:48:44 -0400

    added firebase files

--- app/src/main/java/edu/duke/compsci290/dukefoodapp/MainActivity.java
+++ app/src/main/java/edu/duke/compsci290/dukefoodapp/MainActivity.java
@@ -3,11 +3,27 @@ package edu.duke.compsci290.dukefoodapp;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
+import com.firebase.ui.auth.AuthUI;
+
+import java.util.Arrays;
+
 public class MainActivity extends AppCompatActivity {
+
+
+    // Choose an arbitrary request code value
+    private static final int RC_SIGN_IN = 123;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        startActivityForResult(
+                AuthUI.getInstance()
+                        .createSignInIntentBuilder()
+                        .setAvailableProviders(Arrays.asList(
+                                new AuthUI.IdpConfig.EmailBuilder().build()))
+                        .build(),
+                RC_SIGN_IN);
     }
 }
