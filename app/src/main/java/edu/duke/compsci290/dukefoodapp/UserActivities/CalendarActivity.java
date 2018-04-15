7317618eb73c18a07dc97f40d01783693b2befbc
tevin.brown@duke.edu <tmb51@duke.edu>
2018-04-15 17:42:29 -0400

    need integration with day activity

--- app/src/main/java/edu/duke/compsci290/dukefoodapp/UserActivities/CalendarActivity.java
+++ app/src/main/java/edu/duke/compsci290/dukefoodapp/UserActivities/CalendarActivity.java
@@ -1,72 +1,66 @@
 package edu.duke.compsci290.dukefoodapp.UserActivities;
 
 import android.content.Context;
+import android.content.Intent;
+import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.util.Size;
 import android.view.Gravity;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.BaseAdapter;
+import android.widget.CalendarView;
 import android.widget.GridView;
 import android.widget.TextView;
 
+import com.firebase.ui.auth.data.model.User;
+
+import java.util.Calendar;
+import java.util.Date;
+
 import edu.duke.compsci290.dukefoodapp.R;
+import edu.duke.compsci290.dukefoodapp.model.IUser;
+import edu.duke.compsci290.dukefoodapp.model.UserParent;
 
 public class CalendarActivity extends AppCompatActivity {
-    private String[] mBooks;
-
+    private CalendarView mCalendarView;
+    private UserParent user;
+    private String Tag = "CalendarActivity";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_calendar);
-        mBooks = new String[]{"1","2","3","4","5","6","7","1","2","3","4","5","6","7","1","2","3","4","5","6","7","1","2","3","4","5","6","7","1","2","3","4","5","6","7"};
-        GridView gridView = (GridView)findViewById(R.id.calendar_gridview);
-        BooksAdapter booksAdapter = new BooksAdapter(this, mBooks);
-        gridView.setAdapter(booksAdapter);
-    }
 
-    public class BooksAdapter extends BaseAdapter {
+        //Grab Intent information
+        Intent receivedIntent = this.getIntent();
+        user = (UserParent) receivedIntent.getSerializableExtra("user");
 
-        private final Context mContext;
-        private final String[] books;
 
-        // 1
-        public BooksAdapter(Context context, String[] books) {
-            this.mContext = context;
-            this.books = books;
-        }
-
-        // 2
-        @Override
-        public int getCount() {
-            return books.length;
-        }
-
-        // 3
-        @Override
-        public long getItemId(int position) {
-            return 0;
-        }
-
-        // 4
-        @Override
-        public Object getItem(int position) {
-            return null;
-        }
-
-        // 5
-        @Override
-        public View getView(int position, View convertView, ViewGroup parent) {
-            TextView dummyTextView = new TextView(mContext);
-            dummyTextView.setText(String.valueOf(position));
-            dummyTextView.setBackgroundResource(R.drawable.grid_border);
-            dummyTextView.setTextSize(20);
-            dummyTextView.setGravity(Gravity.CENTER);
-            dummyTextView.setHeight(250);
-            return dummyTextView;
-        }
+        mCalendarView = (CalendarView) findViewById(R.id.calendarView);
+        //TODO: access database and set up color of day based on availability of order.
+//        setUpColors();
+        Date currentTime = Calendar.getInstance().getTime();
+        Log.d(Tag,currentTime.toString());
+        //TODO: set up the Calendar to have fewer months present
+//        mCalendarView.setMinDate();
+//        mCalendarView.setMaxDate();
+        mCalendarView.setOnDateChangeListener(new CalendarView.OnDateChangeListener() {
+            @Override
+            public void onSelectedDayChange(@NonNull CalendarView calendarView, int i, int i1, int i2) {
+                /// want "month day, year"
+                String date = "i + i1 + i2";
+                Log.d("Tag", date);
+                Intent intent = new Intent(CalendarActivity.this, DayActivity.class);
+                intent.putExtra("user",user);
+                intent.putExtra("date", date);
+                startActivity(intent);
+            }
+        });
 
     }
+
+
 }
